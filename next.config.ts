d9a529713aa17b61f52bb1b0f93448f2fb32d274
Hafif Ashiq <hafifashiq07@gmail.com>
2024-12-26 18:56:02 +0500

    Updated build folder

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  distDir: "build", //this line will tell the build to create a file with this name
   images: {
     remotePatterns: [
       {
